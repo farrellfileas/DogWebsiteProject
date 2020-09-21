36acb28a415441ea9e288c1f0328bcda04f4208a
farrellfileas <fileasfarrell@gmail.com>
2020-09-21 11:30:13 -0700

    Remove option tags from HTML, added from JS instead

--- index.js
+++ index.js
@@ -5,8 +5,63 @@
 
 	function init() {
 		document.querySelector("button").addEventListener("click", displayImg);
+		loadOptions();
 	}
 
+	/**
+	  * Request options list from dogs api
+	  */
+	function loadOptions() {
+		var url = "https://dog.ceo/api/breeds/list/all";
+
+		fetch(url)
+		  .then(checkStatus)
+		  .then(resp => resp.json())
+		  .then(options);
+	}
+
+	/**
+	  * Display all dogs as options
+	  */
+	function options(resp) {
+		for (const breed in resp.message) {
+			if (resp.message[breed].length == 0) {
+				var content = capitalize(breed);
+				var value = breed;
+				addOption(content, value);
+			} else {
+				for (const i in resp.message[breed]) {
+					var subBreed = resp.message[breed][i];
+					var content = capitalize(subBreed) + " " + capitalize(breed);
+					var value = breed + "-" + subBreed;
+					addOption(content, value);
+				}
+			}
+		}
+	}
+
+	/**
+	  * Add a single option to the select tag
+	  */
+	function addOption(content, value) {
+		var option = document.createElement("option");
+		option.value = value;
+		option.textContent = content;
+		var select = document.querySelector(".dog-selector");
+
+		select.appendChild(option);
+	}
+
+	/**
+	  * Given string name, capitalize it
+	  */
+	function capitalize(name) {
+		return name.charAt(0).toUpperCase() + name.slice(1);
+	}
+
+	/**
+	  * Requests image of selected dog
+	  */
 	function displayImg() {
 		var e = document.querySelector(".dog-selector");
 		var option = e.options[e.selectedIndex].value;
@@ -31,6 +86,9 @@
 		  .then(display);
 	}
 
+	/**
+	  * Displays the image
+	  */
 	function display(resp) {
 		var img = document.querySelector(".dog-img");
 		img.src = resp.message;
